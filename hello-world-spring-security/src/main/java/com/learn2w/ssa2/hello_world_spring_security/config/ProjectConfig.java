e0e52a19b7e4eaa46c3c6c77cab75bdabfa4e1b8
aurelianF <aurelian.floricica@yahoo.com>
2025-01-31 08:41:38 +0200

    Signed-off-by: aurelianF <aurelian.floricica@yahoo.com>
        Added SecurityFilterChain for basic setup of auth and authrz.

--- hello-world-spring-security/src/main/java/com/learn2w/ssa2/hello_world_spring_security/config/ProjectConfig.java
+++ hello-world-spring-security/src/main/java/com/learn2w/ssa2/hello_world_spring_security/config/ProjectConfig.java
@@ -2,11 +2,15 @@ package com.learn2w.ssa2.hello_world_spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.*;
 
 @Configuration
 public class ProjectConfig {
@@ -23,5 +27,13 @@ public class ProjectConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated());
+        return http.build();
     }
 }
